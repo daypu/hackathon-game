8454864976c4fc8617d63a16c6f4f6cebaaa1cfe
yassay1 <3101074916@qq.com>
2026-06-07 10:32:52 +0800

    chore: keep raft-mini-game only

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './engine/Game.js';
-import { MenuScene } from './scenes/MenuScene.js';
-
-const canvas = document.getElementById('game');
-const game = new Game(canvas);
-game.setScene(new MenuScene(game));
-game.start();
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './',
-  server: {
-    open: false,
-    port: 5173
-  }
-});
